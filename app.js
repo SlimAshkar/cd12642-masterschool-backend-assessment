54c288c8e741b10cba92f166f2f573f365f5592c
Slim Ashkar <slim.ashkar@gmail.com>
2023-06-02 22:10:07 +0300

    Part 1

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+//PART 1
+const app = express();
+const port = 3000;
+app.use(express.json());
+//welcome page for api
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Unsplash API!",
+  });
+});
+
+//app listen
+app.listen(port, () => {
+  console.log(`
+     The app is running,
+     Listening on PORT: ${port}`);
+});
